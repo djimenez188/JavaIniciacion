7a657fe213a2700199306cd230edaca2d26b3943
djimenez188 <rufio88_@outlook.es>
2025-07-27 18:47:36 +0200

    First swing

---
+++ 7.Swing/First_Swing/src/Point.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+    public class Pitoint {
+
+        public static void main(String[] args) {
+            JFrame frame = new JFrame("Formulario");
+            frame.setSize(350, 200);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JPanel panel = new JPanel();
+
+            JLabel nombreLabel = new JLabel("Nombre");
+            JTextField nombreText = new JTextField(15);
+
+            JLabel edadlabel = new JLabel("Edad");
+            JTextField edadText = new JTextField(5);
+
+            JButton enviarBtn = new JButton("Enviar");
+
+            JLabel resultado = new JLabel("");
+
+            enviarBtn.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    String nombre = nombreText.getText();
+                    String edad = edadText.getText();
+                    resultado.setText("Hola " + nombre + ", tienes " + edad + " años.");
+                }
+            });
+            panel.add(nombreLabel);
+            panel.add(nombreText);
+            panel.add(edadlabel);
+            panel.add(edadText);
+            panel.add(enviarBtn);
+            panel.add(resultado);
+
+            frame.add(panel);
+            frame.setVisible(true);
+        }
+    }
+
+
+
